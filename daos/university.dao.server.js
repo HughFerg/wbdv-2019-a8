826bb67101e19d42668688a4bf03eaf861c3bae8
hughferg <hughferguson25@gmail.com>
2019-12-09 13:18:19 -0500

    a8, ig

--- daos/university.dao.server.js
+++ daos/university.dao.server.js
@@ -1,34 +1,56 @@
-
 var students = require('../data/students.json');
 var questions = require('../data/questions.json');
 var answers = require('../data/answers.json');
-function createStudent(student) {
+var studentModel = require('../models/student.model.server');
+
+export function createStudent(student) {
 	students.push(student);
 	return students;
 }
-function deleteStudent(sid) {
-	var index = students.findIndex(student => student._id == sid)
-	students.splice(index, 1)
-	return students;
-}
-function findAllStudents() {
-	return students;
-}
-function findStudentById(sid) {
-	return students.find(student => student._id == sid)
-}
-function findAllQuestions() {
+export function createQuestion(question) {
+	questions.push(question);
 	return questions;
 }
-function findQuestionById(qid) {
+export function answerQuestion(studentId, questionId, answer) {
+	// TODO
+}
+export function updateStudent(student) {
+	// NOT REQUIRED
+}
+export function updateQuestion(question) {
+	// NOT REQUIRED
+}
+export function deleteQuestion(qid) {
+	// NOT REQUIRED
+}
+export function deleteStudent(sid) {
+	// NOT REQUIRED
+}
+export function findStudentByUsername(username) {
+	// return studentModel.find({username: username})
+	return studentModel.findOne({ username: username })
+}
+export function findAllStudents() {
+	return students;
+}
+export function findStudentById(sid) {
+	return students.find(student => student._id == sid)
+}
+export function findAllQuestions() {
+	return questions;
+}
+export function findQuestionById(qid) {
 	return questions.find(question => question._id == qid)
 }
-
-module.exports = {
-	createStudent,
-	deleteStudent,
-	findAllStudents,
-	findStudentById,
-	findAllQuestions,
-	findQuestionById
-};
+export function findAllAnswers() {
+	return answers;
+}
+export function findAnswerById(aid) {
+	return answers.find(a => a._id == aid)
+}
+export function findAnswersByStudent(sid) {
+	// TODO
+}
+export function findAnswersByQuestion(qid) {
+	// TODO
+}
--- services/university.service.server.js
+++ services/university.service.server.js
@@ -1,4 +1,4 @@
-module.exports = function(app) {
+module.exports = function (app) {
 	var universityDao = require('../daos/university.dao.server.js')
 
 	function createStudent(req, res) {
@@ -7,28 +7,48 @@ module.exports = function(app) {
 	}
 	function deleteStudent(req, res) {
 		var sid = req.params['sid'];
-		res.json(universityDao.deleteStudent('sid'))
+		res.json(universityDao.deleteStudent(sid))
+	}
+	function updateStudent(req, res) {
+		const newStudent = req.body;
+		res.json(universityDao.updateStudent(newStudent))
 	}
 	function findAllStudents(req, res) {
 		res.json(universityDao.findAllStudents());
 	}
 	function findStudentById(req, res) {
-		res.json(
-			universityDao.findStudentById(req.params['sid'])
-		)
+		res.json(universityDao.findStudentById(req.params['sid']))
 	}
 	function findAllQuestions(req, res) {
 		res.json(universityDao.findAllQuestions());
 	}
 	function findQuestionById(req, res) {
-		res.json(
-			universityDao.findQuestionById(req.params['qid'])
-		)	
+		res.json(universityDao.findQuestionById(req.params['qid']))
 	}
-	app.post("/api/studentsa", createStudent);
-	app.get("/api/studentsa", findAllStudents);
-	app.get("/api/studentsa/:sid", findStudentById);
-	app.delete("/api/studentsa/:sid", deleteStudent);
-	app.get("/api/questionsa", findAllQuestions);
-	app.get("/api/questionsa/:qid", findQuestionById);
+	function createQuestion(req, res) {
+		res.json(universityDao.createQuestion(req.body))
+	}
+	function updateQuestion(req, res) {
+		res.json(universityDao.updateQuestion(req.body))
+	}
+	function studentAnswerQuestion(req, res) {
+		res.json(universityDao.answerQuestion(req.params['sid'], req.params['qid'], req.body))
+	}
+	
+	app.post("/api/student", createStudent);
+	app.get("/api/student", findAllStudents);
+	app.get("/api/student/:sid", findStudentById);
+	app.put("/api/student/:sid", updateStudent);
+	app.delete("/api/student/:sid", deleteStudent);
+	app.get("/api/question", findAllQuestions);
+	app.post("/api/question", createQuestion);
+	app.get("/api/question/:qid", findQuestionById);
+	app.put("/api/question/:sid", updateQuestion);
+	app.post("/api/student/:sid/question/:qid/answer", studentAnswerQuestion);
+	app.get("/api/answer", findAllAnswers);
+	app.get("/api/answer/:aid", findAnswerById);
+	app.get("/api/question/:qid/answer", findAllAnswersByQuestion);
+	app.get("/api/student/:sid/answer", findAllAnswersByStudent);
+	app.get("/api/student/:sid/question/:qid/answer", findAllAnswersByStudentForQuestion);
+	app.get("/api/question/:qid/student/:sid/answer", findAllAnswersByStudentForQuestion);
 }
